41bc19d0222ead6eabc7bea91f720c11dfa74b17
Armaan Gupta <armaan@kreative.im>
2020-05-27 19:59:55 -0500

    Added updated actions workflow

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ server.use(helmet());
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 
-server.get("/", (req, res) => res.send("dreamflow cms online"));
+server.get("/", (req, res) => res.send("dreamflow cms online mate"));
 server.use(faqs);
 
 DB.authenticate().catch((err) => console.log("Error: " + err))
